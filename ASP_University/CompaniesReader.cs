74b972dfb41788a7b4c07bde6c827fd67004c70a
Vladyslav <kuleba.vlad@gmail.com>
2024-02-10 17:19:29 +0100

    Add one class

---
+++ ASP_University/CompaniesReader.cs
@@ -0,0 +1,19 @@
+﻿public class CompaniesReader
+{
+    private readonly IConfiguration _config;
+
+    public CompaniesReader()
+    {
+        _config = new ConfigurationBuilder()
+            .AddXmlFile("companies.xml", optional: true, reloadOnChange: true)
+            .AddJsonFile("companies.json", optional: true, reloadOnChange: true)
+            .AddIniFile("companies.ini", optional: true, reloadOnChange: true)
+            .Build();
+    }
+
+    public string GetCompanyWithMostEmployees(string fileName)
+    {
+
+        return "Microsoft";
+    }
+}
--- ASP_University/Program.cs
+++ ASP_University/Program.cs
@@ -1,26 +1,20 @@
-using ASP_University;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var config = new ConfigurationBuilder()
-    .AddXmlFile("companies.xml", optional: true, reloadOnChange: true)
-    .AddJsonFile("companies.json", optional: true, reloadOnChange: true)
-    .AddIniFile("companies.ini", optional: true, reloadOnChange: true)
-    .Build();
+var iniName = "companies.ini";
+var jsonName = "companies.json";
+var xmlName = "companies.xml";
 
-// ????????????? IConfiguration ?? ??????
-app.Services.AddSingleton<IConfiguration>(config);
 
-// ????????? ??????? CompanyConfigService ?? ?????????? ???????????
-app.Services.AddSingleton<CompanyConfigService>();
 
 app.MapGet("/", async context =>
 {
-    var companyService = context.RequestServices.GetRequiredService<CompanyConfigService>();
-    var companyWithMostEmployees = companyService.GetCompanyWithMostEmployees();
+    var reader = new CompaniesReader();
+    var companyWithMostEmployeesIni = reader.GetCompanyWithMostEmployees(iniName);
+    var companyWithMostEmployeesJson = reader.GetCompanyWithMostEmployees(jsonName);
+    var companyWithMostEmployeesXml = reader.GetCompanyWithMostEmployees(xmlName);
 
-    await context.Response.WriteAsync($"Company with the most employees: {companyWithMostEmployees}");
+    await context.Response.WriteAsync($"Company with most employees: {companyWithMostEmployeesIni}");
 });
 
 app.Run();
