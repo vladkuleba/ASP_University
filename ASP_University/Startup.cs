6a1160a5374556d40f8af4926b1b40e2e665dbe6
Vladyslav <kuleba.vlad@gmail.com>
2024-02-19 18:22:29 +0100

    Done

--- ASP_University/Startup.cs
+++ ASP_University/Startup.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-
-public class Startup
+﻿public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
